10092c4c7b02760fd3ddf7b2c7a9c3be290a17ec
Marcel Roozekrans <marcel@roozekrans.nl>
2026-03-14 18:51:14 +0100

    fix: add sealed modifier to ParentDocumentOptions

--- src/Rag.NET/Models/Options/ParentDocumentOptions.cs
+++ src/Rag.NET/Models/Options/ParentDocumentOptions.cs
@@ -1,6 +1,6 @@
 namespace Rag.NET.Models.Options;
 
-public class ParentDocumentOptions
+public sealed class ParentDocumentOptions
 {
     public int ParentChunkSize { get; set; } = 2048;
     public int ParentOverlap { get; set; } = 100;
